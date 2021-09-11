666811bc1dae34445ce0e95b5652557dc28066f1
Javier <ibrahimcruz06@gmail.com>
2021-09-10 18:03:39 -0600

    Creacion de la clase base para el programa

---
+++ src/envio.h
@@ -0,0 +1,16 @@
+#ifndef ENVIO_H
+#define ENVIO_H 
+
+#include <string> 
+
+class Envio{
+
+    protected: 
+    Envio(){ }
+
+    public: 
+    ~Envio() {}
+    virtual float CalculoCosto() = 0;  
+}
+
+#endif
